f5e0dbe6d888df01e368a38df563d2ce2e35bdc9
Roopesh Nair <roopesh90nair@live.com>
2013-05-16 18:56:42 +0530

    exit code updated

--- www/js/index.js
+++ www/js/index.js
@@ -145,9 +145,9 @@ function doMenu() {
 
 // This function exits the app.
 function onExit(){
-	alert("close");
+	//alert("close");
         navigator.device.exitApp();
-        return false;
+        //return false;
 }
 
 
